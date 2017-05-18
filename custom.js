ab097100278d80978b0c3103829e515702865f73
Ashraful Kabir <ashraful_info@yahoo.com>
2017-05-18 16:16:53 -0400

    Update custom.js

--- custom.js
+++ custom.js
@@ -2,14 +2,18 @@
 var pageCounter = 1;
 var animalContainer = document.getElementById("animal-info");
 var btn = document.getElementById("btn");
-
+/* while button click */
 btn.addEventListener("click", function() {
+  /* xml http request */
   var ourRequest = new XMLHttpRequest();
+  /* get json data from external url */
   ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
     
   ourRequest.onload = function() {
     if (ourRequest.status >= 200 && ourRequest.status < 400) {
+      /* parsing json data */
       var ourData = JSON.parse(ourRequest.responseText);
+      /* calling the html render function */
       renderHTML(ourData);
     } else {
       console.log("We connected to the server, but it returned an error.");
@@ -30,10 +34,12 @@ btn.addEventListener("click", function() {
     
 });
 
+/* renderHTML Funcion */
 function renderHTML(data) {
   var htmlString = "";
 
   for (i = 0; i < data.length; i++) {
+    /* html string to add content to the animal container  */
     htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
     
     for (ii = 0; ii < data[i].foods.likes.length; ii++) {
@@ -55,7 +61,7 @@ function renderHTML(data) {
     }
 
     htmlString += '.</p>';
-
+    /* html string ends */
   }
 
   animalContainer.insertAdjacentHTML('beforeend', htmlString);
